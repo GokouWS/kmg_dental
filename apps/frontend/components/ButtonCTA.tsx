972003680de30ce6869fa954f4a0cca0cda5fb64
GokouWS <lynxyyeu@gmail.com>
2025-06-12 00:16:01 +0100

    feat: update CTA button styling

--- apps/frontend/components/ButtonCTA.tsx
+++ apps/frontend/components/ButtonCTA.tsx
@@ -4,7 +4,7 @@ interface ButtonCTAProps {
 
 const ButtonCTA = ({text}: ButtonCTAProps) => {
   return (
-    <button className="bg-linear-to-b from-pink-light to-pink-dark rounded-button py-3 px-5">
+    <button className="bg-linear-to-b from-pink-light to-pink-dark rounded-button py-3 px-5 text-white text-nowrap">
       {text}
     </button>
   );
