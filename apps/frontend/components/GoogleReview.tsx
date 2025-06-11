69ad6daa4b1c5fc52e66f707ae04882419da59fa
GokouWS <lynxyyeu@gmail.com>
2025-06-12 00:29:55 +0100

    feat: update google review component

--- apps/frontend/components/GoogleReview.tsx
+++ apps/frontend/components/GoogleReview.tsx
@@ -1,5 +1,16 @@
-const GoogleReview = () => {
-  return <div>Google Review</div>;
+interface GoogleReviewProps {
+  rating: string;
+}
+
+const GoogleReview = ({rating}: GoogleReviewProps) => {
+  return (
+    <div className="w-full bg-primary-mid-pink max-h-[134px] md:place-self-end h-[9.3vw] md:-ml-[2vw] z-20 text-right flex flex-col justify-center">
+      <div></div>
+      <p>
+        RATED <span className="text-[44px]">{rating}</span> GOOGLE REVIEWS
+      </p>
+    </div>
+  );
 };
 
 export default GoogleReview;
