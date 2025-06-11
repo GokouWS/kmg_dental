83a3ff1264b5073fc53cac0f1d5c53cd8580d28b
GokouWS <lynxyyeu@gmail.com>
2025-06-12 00:29:18 +0100

    feat: from box update

--- apps/frontend/components/FromBox.tsx
+++ apps/frontend/components/FromBox.tsx
@@ -1,5 +1,16 @@
-const FromBox = () => {
-  return <div>From Box</div>;
+interface FromBoxProps {
+  textTop: string;
+  textBottom: string;
+}
+
+const FromBox = ({textTop, textBottom}: FromBoxProps) => {
+  return (
+    <div className="bg-primary-dark-grey rounded-[110px] max-h-[356px] max-w-[110px] h-[24.7vw] w-[7.6vw] flex flex-col text-center items-center justify-center md:gap-5 z-30 text-white">
+      <p>{textTop}</p>
+      <div className="max-h-[112px] h-full w-px border border-solid border-primary-mid-pink" />
+      <p>{textBottom}</p>
+    </div>
+  );
 };
 
 export default FromBox;
