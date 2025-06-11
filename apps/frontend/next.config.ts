dd7a6eb997a7d53e6c04758226280dec8b2726fd
GokouWS <lynxyyeu@gmail.com>
2025-06-11 04:00:02 +0100

    fix: allow images from wordpress url in next config

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,6 +1,16 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "/wp-content/uploads/**",
+      },
+    ],
+  },
   /* config options here */
 };
 
